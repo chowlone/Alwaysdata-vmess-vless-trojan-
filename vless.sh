9500f73886cc070e9e0e2201baa2158068cb6f10
bhqz <64308144+bhqz@users.noreply.github.com>
2022-05-19 14:04:57 +0800

    Add files via upload

---
+++ vless.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+rm -f web config.json
+wget -N https://raw.githubusercontent.com/Misaka-blog/AX/main/web
+chmod +x ./web
+
+if [[ -z $id ]]; then
+    id="1eb6e917-774b-4a84-aff6-b058577c60a5"
+fi
+
+cat <<EOF > ~/config.json
+{
+    "log": {
+        "loglevel": "warning"
+    },
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$id"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+./web -config=config.json
